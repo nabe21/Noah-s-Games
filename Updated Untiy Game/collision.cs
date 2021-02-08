5ae97d2863b8b819f5b803d28a08c08b166404f5
nabe21 <77988984+nabe21@users.noreply.github.com>
2021-02-07 20:56:53 -1000

    Add files via upload

---
+++ Updated Untiy Game/collision.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collision : MonoBehaviour{
+    Vector3 originalPos;
+
+    void Start()
+    {
+        gameObject.tag = "Player";
+        originalPos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+
+    }
+    private void OnTriggerEnter2D(Collider2D other){
+        if (other.tag == "Collider")
+        {
+            Debug.Log("hit detected");
+            transform.localScale = new Vector3(1, 1, 1);
+            gameObject.transform.position = originalPos;
+
+        }
+
+
+    }
+
+}
